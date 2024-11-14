926cb0e99419b568809724f71d6ad698e83191da
mins-git <m2insapp@gmail.com>
2024-11-14 11:18:14 +0900

    faet(vue): Vue with DRF02

---
+++ day13 - Vue with DRF02/vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink :to="{ name: 'ArticleView' }">Articles</RouterLink> |
+      <RouterLink :to="{ name: 'SignUpView' }">SignUpView</RouterLink> |
+      <RouterLink :to="{ name: 'LogInView' }">LogInView</RouterLink>
+
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
